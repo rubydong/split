869572ab4cd5a7f06f49ee0f8b41d11d5ed12337
Ruby Dong <ruby@macbook-air.lan>
2022-05-14 23:03:28 -0400

    calculate based on subtotal and total only

--- docs/split.js
+++ docs/split.js
@@ -1,231 +1,130 @@
-const individualTotalPlaceholder = 'Individual Total ($)';
-const onInputCall = 'this.value=validateNumber(this.value)';
+const INDIVIDUAL_SUBTOTAL_PLACEHOLDER = "Individual Subtotal ($)";
+const ADDITIONAL_EXPENSE_PLACEHOLDER = "Additional Expense ($)";
+const onInputChange = "this.value=validateNumber(this.value)";
 
-function allocateIndividualInputs() {
-  const splitEvenly =
-    document.querySelector('#split-evenly').value === 'Yes' ? true : false;
+const allocateIndividualInputs = () => {
   let individualTotalsSection = document.querySelector(
-    '#individual-totals-section'
+    "#individual-totals-section"
   );
-  individualTotalsSection.innerHTML = '';
+  individualTotalsSection.innerHTML = "";
 
-  let baseAmountSection = document.querySelector('#base-amount-section');
-  if (splitEvenly) {
-    baseAmountSection.innerHTML = `
-      What is the amount before tips and taxes? <br/>
-      <input id="base-amount" class="padding-top" type="text" oninput="${onInputCall}">
-    `;
-    return;
-  }
-
-  baseAmountSection.innerHTML = '';
-  const total = document.querySelector('#amount-of-people-input').value;
+  const total = document.querySelector("#amount-of-people-input").value;
 
   for (let i = 1; i <= total; i++) {
     individualTotalsSection.innerHTML += `
       <div class="fieldInputRow" id="individualTotalRow-${i}">
         <input type="text" id="person-name-${i}" class="light-spacing" placeholder="Person ${i}"/>
-        <input type="text" class="person-${i}-expense" class="light-spacing" placeholder="${individualTotalPlaceholder}" oninput="${onInputCall}"/>
+        <input type="text" class="person-${i}-expense" class="light-spacing" placeholder="${INDIVIDUAL_SUBTOTAL_PLACEHOLDER}" oninput="${onInputChange}"/>
         <button class="additionalExpenseButton" onclick="addAdditionalExpense(${i})">+</button>  
       </div>
       <div class="additionalExpensesSection" id="additionalExpensesSection-${i}"></div>
       <p/>
     `;
   }
-}
+};
 
 allocateIndividualInputs();
 
-function calculate() {
-  const taxAmount = parseFloat(
-    document.querySelector('#tax-amount').value || 0
-  );
-  const tipAmount = parseFloat(
-    document.querySelector('#tip-amount').value || 0
-  );
-  const isTipBasedOnPretax =
-    document.querySelector('input[name="tipTaxPreference"]:checked').value ===
-      'preTax' || isTipsPercentageSectionHidden();
-
-  let results = document.querySelector('#results');
+const calculate = () => {
+  let results = document.querySelector("#results");
   // clear existing calculations for results
-  results.innerHTML = '<h3>Calculations</h3> <p/>';
+  results.innerHTML = "<h3>Calculations</h3> <p/>";
 
-  if (Number.isNaN(taxAmount) || Number.isNaN(tipAmount)) {
-    results.innerHTML += '<u>Please enter valid tax or tip amounts.</u>';
+  const numOfPeople = document.querySelector("#amount-of-people-input").value;
+  const totalAmount = parseFloat(document.querySelector("#total").value);
+
+  if (Number.isNaN(totalAmount)) {
+    results.innerHTML = "<u>Please enter a total.</u>";
     return;
   }
+  let overallSubtotal = 0;
+  const individualTotalMap = {};
+  for (let i = 1; i <= numOfPeople; i++) {
+    let individualSum = 0;
+    const individualExpenses = document.querySelectorAll(
+      `.person-${i}-expense`
+    );
+    individualExpenses.forEach((expense) => {
+      individualSum += parseFloat(expense.value) || 0;
+    });
+    individualTotalMap[i] = individualSum;
+    overallSubtotal += individualSum;
+  }
 
-  const taxType = document.querySelector('#tax-type').value;
-  const tipType = document.querySelector('#tip-type').value;
+  for (let i = 1; i <= numOfPeople; i++) {
+    const personName =
+      document.querySelector(`#person-name-${i}`).value || `Person ${i}`;
+    const individualSum = individualTotalMap[i];
+    if (Number.isNaN(individualSum) || individualSum === 0) {
+      results.innerHTML = "<u>Please enter valid individual totals.</u>";
+      return;
+    }
 
-  const splitEvenly =
-    document.querySelector('#split-evenly').value === 'Yes' ? true : false;
-  const numOfPeople = document.querySelector('#amount-of-people-input').value;
-
-  if (splitEvenly) {
-    // splitting the bill evenly
-    const baseAmount = parseFloat(document.querySelector('#base-amount').value);
-    const taxMultiplier = getMultiplier(baseAmount, taxType, taxAmount);
-    const tipMultiplier = getMultiplier(baseAmount, tipType, tipAmount);
-
-    let total = 0;
-    let multiplier = isTipBasedOnPretax
-      ? taxMultiplier + tipMultiplier + 1
-      : (taxMultiplier + 1) * (tipMultiplier + 1);
-
-    total = baseAmount * multiplier;
-    if (Number.isNaN(total)) {
-      results.innerHTML += '<u>Please enter a valid base amount total.</u>';
+    const individualTotal = getIndividualTotal(
+      individualSum,
+      overallSubtotal,
+      totalAmount
+    );
+    if (Number.isNaN(individualTotal)) {
+      results.innerHTML = "<u>Please enter valid individual totals.</u>";
       return;
     }
 
     results.innerHTML += `
-      The total is <u>$${total.toFixed(2)}</u> 
-      and the amount evenly split among ${numOfPeople} people is 
-      <u>$${(total / numOfPeople).toFixed(2)}</u>.
-    `;
-  } else {
-    // splitting the bill by individual totals
-    let sum = 0;
-    const individualTotalMap = {};
-    for (let i = 1; i <= numOfPeople; i++) {
-      let individualSum = 0;
-      let individualExpenses = document.querySelectorAll(
-        `.person-${i}-expense`
-      );
-      individualExpenses.forEach((expense) => {
-        individualSum += parseFloat(expense.value) || 0;
-      });
-      individualTotalMap[i] = individualSum;
-      sum += individualSum;
-    }
-
-    const taxMultiplier = getMultiplier(sum, taxType, taxAmount);
-    const tipMultiplier = getMultiplier(sum, tipType, tipAmount);
-
-    const multiplier = isTipBasedOnPretax
-      ? taxMultiplier + tipMultiplier + 1
-      : (taxMultiplier + 1) * (tipMultiplier + 1);
-
-    let overallTotal = 0;
-    let overallTipAmt = 0;
-    let overallTaxAmt = 0;
-
-    for (let i = 1; i <= numOfPeople; i++) {
-      let personName =
-        document.querySelector(`#person-name-${i}`).value || `Person ${i}`;
-      let personTotal = individualTotalMap[i];
-      if (Number.isNaN(personTotal)) {
-        results.innerHTML = '<u>Please enter valid individual totals.</u>';
-        return;
-      }
-
-      let individualTaxAmount = (personTotal * taxMultiplier).toFixed(2);
-      let individualTotal = (personTotal * multiplier).toFixed(2);
-      let individualTipAmount = (
-        individualTotal -
-        individualTaxAmount -
-        personTotal
-      ).toFixed(2);
-
-      overallTotal += parseFloat(individualTotal);
-      overallTipAmt += parseFloat(individualTipAmount);
-      overallTaxAmt += parseFloat(individualTaxAmount);
-
-      results.innerHTML += `
         <b>${personName}</b> will pay 
         <u>$${individualTotal}</u>  
-        <span class="grayText">(tax: $${individualTaxAmount} + tip: $${individualTipAmount})</span>
         <p/>
       `;
-    }
-
-    results.innerHTML += `
-      Total: <u>$${overallTotal.toFixed(2)}</u> 
-      <span class="grayText">
-        (Subtotal: $${sum.toFixed(2)} + 
-        Taxes: $${overallTaxAmt.toFixed(2)} +
-        Tips: $${overallTipAmt.toFixed(2)})
-      </span>
-    `;
   }
-}
+};
 
-// Generic number helpers
-function getMultiplier(baseAmount, type, amount) {
-  // this determines what the multipler is for the tax and tip
-  if (type === '$') {
-    return (baseAmount + amount) / baseAmount - 1;
-  }
-  return amount * 0.01;
-}
+const getIndividualTotal = (individualSubtotal, subtotal, total) => {
+  return ((individualSubtotal / subtotal) * total).toFixed(2);
+};
 
-function validateNumber(value) {
+const validateNumber = (value) => {
   // only allows numbers and one decimal at most
-  return value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-}
+  return value.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1");
+};
 
-// dealing with additional expenses
-function generateRandomString() {
-  return 'expense' + Math.random().toString(36).substring(2, 7);
-}
+// id for dealing with additional expenses
+const generateRandomString = () => {
+  return "expense" + Math.random().toString(36).substring(2, 7);
+};
 
-function addAdditionalExpense(person) {
+const addAdditionalExpense = (person) => {
   const randomString = generateRandomString();
-  document.querySelector(`#additionalExpensesSection-${person}`).innerHTML += `
-      <p/>
-      <div class="fieldInputRow" id="${randomString}">  
-        <input 
-          type="text" 
-          class="person-${person}-expense" 
-          id="${randomString}-input" 
-          placeholder="${individualTotalPlaceholder}" 
-          oninput="${onInputCall}"
-        />
-        <button 
-          class="removeExpenseButton" 
-          onclick="removeAdditionalExpense('#${randomString}', '#additionalExpensesSection-${person}')"
-        >
-          -
-        </button>
-      </div>
-    `;
-}
+  const div = document.createElement("div");
+  div.classList.add("fieldInputRow");
+  div.id = randomString;
 
-function removeAdditionalExpense(randomString) {
+  const input = document.createElement("input");
+  input.type = "text";
+  input.classList.add(`person-${person}-expense`);
+  input.id = `${randomString}-input`;
+  input.placeholder = ADDITIONAL_EXPENSE_PLACEHOLDER;
+  input.setAttribute("oninput", onInputChange);
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("removeExpenseButton");
+  removeButton.onclick = () => removeAdditionalExpense(`#${randomString}`);
+  removeButton.textContent = "-";
+
+  div.appendChild(input);
+  div.appendChild(removeButton);
+
+  let p = document.createElement("p");
+
+  const additionalExpensesForPerson = document.querySelector(
+    `#additionalExpensesSection-${person}`
+  );
+  additionalExpensesForPerson.append(p);
+  additionalExpensesForPerson.append(div);
+};
+
+const removeAdditionalExpense = (randomString) => {
   const currNode = document.querySelector(randomString);
   if (currNode) {
     currNode.parentElement.removeChild(currNode);
   }
-}
-
-// Tips
-function changeTipAmount(tipAmount) {
-  document.querySelector('#tip-amount').value = tipAmount;
-}
-
-function toggleTipsPercentagesSection() {
-  // do not show this section if user selects $ for the tips preference
-  if (isTipsPercentageSectionHidden()) {
-    document.querySelector('.tipsBasedOnPercentages').style.display = 'block';
-  } else {
-    document.querySelector('.tipsBasedOnPercentages').style.display = 'none';
-  }
-}
-
-function isTipsPercentageSectionHidden() {
-  return (
-    document.querySelector('.tipsBasedOnPercentages').style.display === 'none'
-  );
-}
-
-// Taxes
-function defaultTaxPercentage() {
-  const nycTax = 8.875;
-  if (document.querySelector('#tax-type').value === '%') {
-    document.querySelector('#tax-amount').value = nycTax;
-  } else {
-    document.querySelector('#tax-amount').value = null;
-  }
-}
+};
