04a8b8b610c016340d0954f33b6e69f38511b418
Ruby Dong <ruby@macbook-air.lan>
2024-10-23 16:57:31 -0400

    Updates

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const INTRO_SCREEN = "introScreen";
-export const EXPENSES_SCREEN = "expensesScreen";
-export const RESULTS_SCREEN = "resultsScreen";
-export const SELECT_PLACEHOLDER = "Select People";
